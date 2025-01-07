dd0054e48748abbe90079e52f768ae3f6456543c
Alia bint Bazeel <aliabazeel123@gmail.com>
2025-01-07 11:27:52 +0400

    frontend

--- Frontend_main/script.js
+++ Frontend_main/script.js
@@ -6,13 +6,17 @@ const headerH1 = document.querySelector('.fixed-header h1');
 const headerH4 = document.querySelector('.fixed-header h4');
 const logo = document.querySelector('.logo');
 const navLinks = document.querySelectorAll('.nav-link');
+const dropdownLinks = document.querySelectorAll('.dropdown-menu .nav-link');
 
-// Ensure smooth transition for header, text, and logo changes
+// Ensuring smooth transition for header, text, and logo changes
 header.style.transition = 'all 0.3s ease-in-out';
 headerH1.style.transition = 'font-size 0.3s ease-in-out';
 headerH4.style.transition = 'font-size 0.3s ease-in-out';
 logo.style.transition = 'width 0.3s ease-in-out, height 0.3s ease-in-out';
 navLinks.forEach(link => {
+    link.style.transition = 'font-size 0.3s ease-in-out';
+});
+dropdownLinks.forEach(link => {
     link.style.transition = 'font-size 0.3s ease-in-out';
 });
 
@@ -25,8 +29,10 @@ const defaultFontSizeH4 = '3em';
 const shrinkFontSizeH4 = '1.5em';
 const defaultLogoSize = '150px';
 const shrinkLogoSize = '75px';
-const defaultNavFontSize = '1.75em';
+const defaultNavFontSize = '1.2em';
 const shrinkNavFontSize = '1em';
+const defaultDropdownFontSize = '1em';
+const shrinkDropdownFontSize = '0.8em';
 
 // HEADER SHRINK ON SCROLL 
 window.addEventListener('scroll', () => {
@@ -40,6 +46,9 @@ window.addEventListener('scroll', () => {
         navLinks.forEach(link => {
             link.style.fontSize = shrinkNavFontSize;
         });
+        dropdownLinks.forEach(link => {
+            link.style.fontSize = shrinkDropdownFontSize;
+        });
     } else {
         // Restore default header, font sizes, logo, and nav links
         header.style.padding = defaultPadding;
@@ -49,6 +58,9 @@ window.addEventListener('scroll', () => {
         logo.style.height = 'auto';
         navLinks.forEach(link => {
             link.style.fontSize = defaultNavFontSize;
+        });
+        dropdownLinks.forEach(link => {
+            link.style.fontSize = defaultDropdownFontSize;
         });
     }
 });
